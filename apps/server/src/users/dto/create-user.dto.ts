102447107302333e5f09e24b7bb8d82466480362
guebebraga <guebe.braga5@gmail.com>
2024-10-11 21:03:01 -0300

    refactor: create-user.dto was modified

--- apps/server/src/users/dto/create-user.dto.ts
+++ apps/server/src/users/dto/create-user.dto.ts
@@ -2,4 +2,8 @@ export class CreateUserDto {
     readonly name: string;
     readonly email: string;
     readonly password: string;
+    readonly  mobileNumber: string;
+    readonly birthDate: Date;
+    readonly nationality: string;
+
 }
