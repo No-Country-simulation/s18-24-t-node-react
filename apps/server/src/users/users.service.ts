ad1b39baa30b2b9b27efba1c071092612da4e068
isakidev <gaspar.c.developer@gmail.com>
2024-10-14 23:55:24 -0300

    feat: add user service

---
+++ apps/server/src/users/users.service.ts
@@ -0,0 +1,60 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { InjectModel } from '@nestjs/mongoose';
+import { JwtService } from '@nestjs/jwt';
+
+import { Model } from 'mongoose';
+import { compare } from 'bcrypt';
+
+import { LoginUserDto } from './dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) { }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.findOneByEmail(email);
+
+    const isPasswordMatch = await compare(password, user.password);
+
+    if (!isPasswordMatch)
+      throw new UnauthorizedException('Email or password incorrect');
+
+    const token = this.generateJwt({ id: user.id });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+        mobileNumber: user.mobileNumber,
+        birthDate: user.birthDate,
+        nationality: user.nationality,
+      },
+      token,
+    };
+  }
+
+  async findOneByEmail(email: string) {
+    const user = await this.userModel.findOne({ email });
+
+    if (!user)
+      throw new NotFoundException(`User with email ${email} not exists`);
+
+    return user;
+  }
+
+  private generateJwt(data: { id: string }): string {
+    const token = this.jwtService.sign(data);
+    return token;
+  }
+}
