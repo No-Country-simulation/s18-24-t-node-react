aa208f66ecf2dba3b4e06670524e8c6f9db8d037
isakidev <gaspar.c.developer@gmail.com>
2024-10-14 23:54:30 -0300

    feat: add mongoose and import in app module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://admin:adminpassword@localhost:27017'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
