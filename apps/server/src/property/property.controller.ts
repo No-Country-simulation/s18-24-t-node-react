57e5e7feb1bc4d0611b960f0a6510510efcc0c71
Joaquin <joaquin.butera@gmail.com>
2024-10-17 20:07:46 -0300

    fix: added missing http methods

--- apps/server/src/property/property.controller.ts
+++ apps/server/src/property/property.controller.ts
@@ -1,4 +1,3 @@
-import { PropertyParamsDto } from './dto/property-params.dto';
 import {
   Controller,
   Post,
@@ -17,6 +16,7 @@ import { Property } from './entities/property.entity';
 import { ImageService } from './image.service';
 import { CreatePropertyDto, UpdatePropertyDto } from './dto';
 import { ObjectIdValidationPipe } from 'src/common/pipes/object-id-validation.pipe';
+import { PropertyParamsDto } from './dto/property-params.dto';
 
 @Controller('property')
 export class PropertyController {
@@ -25,15 +25,49 @@ export class PropertyController {
     private readonly propertyService: PropertyService,
   ) { }
 
+  @Get()
+  async findAll(@Query() filterQuery: PropertyParamsDto) {
+    return await this.propertyService.findAll(filterQuery);
+  }
+
   @Get(':id')
   async find(
     @Param('id', ObjectIdValidationPipe) propertyId: string,
   ): Promise<Property> {
     return this.propertyService.findOneById(propertyId);
   }
-  
-  @Get()
-  async findAll(@Query() filterQuery: PropertyParamsDto) {
-    return await this.propertyService.findAll(filterQuery);
+
+  @Post('register')
+  @UseInterceptors(FilesInterceptor('images'))
+  async create(
+    @UploadedFiles() files: Express.Multer.File[],
+    @Body() createPropertyDto: CreatePropertyDto,
+  ): Promise<Property> {
+    const imageUrls = await this.imageService.uploadImages(files);
+
+    const propertyData = {
+      ...createPropertyDto,
+      photos: imageUrls,
+    };
+
+    return this.propertyService.create(propertyData);
+  }
+
+  @Get('get/:id')
+  async findOneById(@Param('id') id: string) {
+    return this.propertyService.findOneById(id);
+  }
+
+  @Delete('delete/:id')
+  async remove(@Param('id') id: number) {
+    return this.propertyService.remove(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ObjectIdValidationPipe) propertyId: string,
+    @Body() updatePropertyDto: UpdatePropertyDto,
+  ): Promise<Property> {
+    return this.propertyService.update(propertyId, updatePropertyDto);
   }
 }
