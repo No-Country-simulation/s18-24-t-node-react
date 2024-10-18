60c86f186c15021e1af1ba30b2a63f57af38ee5e
guebebraga <guebe.braga5@gmail.com>
2024-10-18 17:12:00 -0300

    feat: implement swagger on property and user

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Booked')
+    .setDescription('Application for house reservations')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Global pipes for validation
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -15,9 +26,9 @@ async function bootstrap() {
   );
 
   const PORT = process.env.PORT || 3001;
-
+  
   await app.listen(PORT);
-
   Logger.log(`App listen in: http://localhost:${PORT}`);
 }
+
 bootstrap();
