ee7d592fa27d2f6875745dd182b48b3f8dd89c3f
isakidev <gaspar.c.developer@gmail.com>
2024-10-19 19:32:21 -0300

    ci: update workflow

--- apps/server/src/property/image.service.ts
+++ apps/server/src/property/image.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import cloudinary from '../cloudinaryConfig';
 
-import { Multer } from 'multer';
-
 @Injectable()
 export class ImageService {
   async uploadImages(files: Express.Multer.File[]): Promise<string[]> {
