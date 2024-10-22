73d166e2898c2d3928e4b66caa351649340dcb4a
guebebraga <guebe.braga5@gmail.com>
2024-10-22 17:11:29 -0300

    feat: implement swagger in production

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,25 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as basicAuth from 'express-basic-auth';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle('Booked')
-    .setDescription('Application for house reservations')
-    .setVersion('1.0')
-    .build();
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   // Global pipes for validation
-
   app.enableCors();
-
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -29,10 +20,33 @@ async function bootstrap() {
     }),
   );
 
+  // Configuración de Swagger en todos los entornos
+  const config = new DocumentBuilder()
+    .setTitle('Booked')
+    .setDescription('Application for house reservations')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Condicionar Swagger con autenticación básica solo en producción
+  if (process.env.NODE_ENV === 'production') {
+    app.use(
+      ['/api'],  // Rutas que deseas proteger (Swagger en este caso)
+      basicAuth({
+        users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD },
+        challenge: true,  // Activa el desafío del navegador
+      }),
+    );
+  }
+
+  // Configurar Swagger en la ruta '/api'
+  SwaggerModule.setup('api', app, document);
+
   const PORT = process.env.PORT || 3001;
-  
   await app.listen(PORT);
-  Logger.log(`App listen in: http://localhost:${PORT}/api`);
+  Logger.log(`App listening on: http://localhost:${PORT}/api`);
 }
 
 bootstrap();
+
