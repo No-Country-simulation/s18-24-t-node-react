2c54962694bbc1b34a381c14af9d2a20ed0a27e6
guebebraga <guebe.braga5@gmail.com>
2024-10-26 20:19:28 -0300

    feat: implement payment page

--- apps/web/src/app/api/callApi.js
+++ apps/web/src/app/api/callApi.js
@@ -10,3 +10,42 @@ export async function userRegister(newUser) {
   });
   return response;
 }
+
+export async function paymentStripe(newPayment) {
+  // Convertimos unitAmount y quantity a números antes de enviarlos
+  const sanitizedPayment = {
+    ...newPayment,
+    unitAmount: Number(newPayment.unitAmount),
+    quantity: Number(newPayment.quantity),
+  };
+
+  try {
+    const response = await fetch(`${API}/payments/createPaymentSession`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sanitizedPayment),
+    });
+
+    if (!response.ok) {
+      console.error("Error al registrar el pago", response.status);
+      return null;
+    }
+
+    const data = await response.json();
+
+    if (data && data.url) {
+      return data.url;
+    } else {
+      console.error(
+        "Error al registrar el pago: No se recibió una URL",
+        response.status
+      );
+      return null;
+    }
+  } catch (error) {
+    console.error("Error al registrar el pago", error);
+    return null;
+  }
+}
---
+++ apps/web/src/app/payment/page.js
@@ -0,0 +1,169 @@
+"use client";
+import { useState, useEffect } from "react";
+import { paymentStripe } from "@/app/api/callApi";
+import { AlertPopup } from "@/app/components/Alert";
+import { HeaderBooked } from "@/app/components/headerBooked";
+
+export default function PaymentForm() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [unit_Amount, setUnitAmount] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [alert, setAlert] = useState({ show: false, message: "", type: "" });
+
+  useEffect(() => {}, []);
+
+  function handleChangeName(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+  function handleChangeDescription(e) {
+    e.preventDefault();
+    setDescription(e.target.value);
+  }
+  function handleChangeCurrency(e) {
+    e.preventDefault();
+    setCurrency(e.target.value);
+  }
+  function handleChangeUnitAmount(e) {
+    e.preventDefault();
+    setUnitAmount(e.target.value);
+  }
+  function handleChangeQuantity(e) {
+    e.preventDefault();
+    setQuantity(e.target.value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    let newFormData = {
+      name: "",
+      description: "",
+      currency: "",
+      unit_Amount: 0,
+      quantity: 1,
+      success_url: "http://localhost:3000/success",
+      /*cancel_url:"http://localhost:3000/cancel",*/
+    };
+    for (const [key, value] of formData.entries()) {
+      newFormData[key] =
+        key === "unit_Amount" || key === "quantity" ? Number(value) : value;
+    }
+    console.log(newFormData);
+    const response = await paymentStripe(newFormData);
+    if (response === null) {
+      setAlert({
+        show: true,
+        message: "Error al registrar el pago",
+        type: "error",
+      });
+    }
+
+    if (response.ok) {
+      setAlert({
+        show: true,
+        message: "Registro exitoso",
+        type: "success",
+      });
+      window.location.href = response.url;
+    } else {
+      throw new Error("Error al registrar el pago", response.status);
+    }
+    setTimeout(
+      () =>
+        setAlert({
+          show: false,
+          message: "",
+          type: "",
+        }),
+      10000
+    );
+  }
+  return (
+    <div className="flex flex-col items-center bg-white h-[750px] text-black">
+      <HeaderBooked />
+      <div className="justify-start	justify-content: flex-start">
+        <h2 className="font-semibold text-4xl my-2 ml-2">Registrar mi pago</h2>
+        <p>
+          {" "}
+          Ingresa tus datos para hacer efectiva tu reserva y pagarla de forma
+          rapida y segura
+        </p>
+      </div>
+      <div
+        className="w-1/3 min-w-[700px] h-[80%] rounded-lg border-[#318F51] my-4 shadow-md bg-[#5FA77738]"
+        id="formPayment"
+      >
+        {alert.show && <AlertPopup message={alert.message} type={alert.type} />}
+
+        <form
+          className="px-8 mx-12 flex flex-col items-end"
+          onSubmit={handleSubmit}
+          id="register"
+        >
+          <label className="block py-1 w-[100%] font-medium">Name*</label>
+          <input
+            className="block p-1 w-[100%] rounded"
+            name="name"
+            type="text"
+            placeholder="Name"
+            onChange={handleChangeName}
+            required
+          ></input>
+          <label className="block py-1 w-[100%] font-medium">
+            Description*
+          </label>
+          <input
+            className="block p-1 w-[100%] rounded"
+            name="description"
+            type="text"
+            placeholder="Escribe aqui la descripcion"
+            title=""
+            onChange={handleChangeDescription}
+            required
+          ></input>
+          <label className="block py-1 w-[100%] font-medium">Currency*</label>
+          <div className="block w-[100%] rounded">
+            <input
+              className="p-1 w-[80%] rounded"
+              onChange={handleChangeCurrency}
+              name="currency"
+              type="text"
+              placeholder="Ingresa aqui la moneda"
+              required
+            ></input>
+          </div>
+          <label className="block py-1 w-[100%] font-medium">UnitAmount*</label>
+          <input
+            className="block p-1 w-[100%] rounded"
+            name="unit_Amount"
+            type="number"
+            minLength={14}
+            maxLength={14}
+            placeholder="Ej:1110"
+            title="Unit_Amount"
+            required
+            onChange={handleChangeUnitAmount}
+          ></input>
+          <label className="block py-1 w-[100%] font-medium">Quantity*</label>
+          <input
+            className="block p-1 w-[100%] rounded"
+            onChange={handleChangeQuantity}
+            name="quantity"
+            type="number"
+            placeholder="Elije una cantidad"
+            required
+          ></input>
+          <button
+            className=" block bg-[#318F51] text-white rounded w-[124px] h-[40px] my-2 "
+            type="submit"
+          >
+            Crear pago
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
