8b4bc2acfd78f8614c53bc3f26098d1b794ed4d0
isakidev <gaspar.c.developer@gmail.com>
2024-10-27 21:12:32 -0300

    feat: create grid properties component

---
+++ apps/web/src/components/GridProperties.js
@@ -0,0 +1,10 @@
+import Image from "next/image"
+import { CardProperty } from "./CardProperty"
+
+export const GridProperties = ({ properties }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
+      {properties?.map(property => <CardProperty key={property.id} property={property}/>)}
+    </div>
+  )
+}
