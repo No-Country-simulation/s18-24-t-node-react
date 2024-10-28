4530199e6e10262c9ca7637dce80ea722d44816f
isakidev <gaspar.c.developer@gmail.com>
2024-10-27 21:13:52 -0300

    feat: crear properties hook

---
+++ apps/web/src/hooks/useProperties.js
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from "react"
+
+export const useProperties = () => {
+  const [properties, setProperties] = useState([])
+  const [isLoading, setIsLoading,] = useState(true)
+
+
+  const getProperties = async () => {
+    setIsLoading(true)
+
+    try {
+      const data = await fetch('http://localhost:3001/property')
+      if (!data.ok) throw new Error('Error')
+
+      const propertiesData = await data.json()
+
+      setProperties(propertiesData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return {
+    getProperties,
+    properties,
+    isLoading
+  }
+}
