a2aa6c71f0fb6c2c21c3bf0335b338a7542fc989
isakidev <gaspar.c.developer@gmail.com>
2024-10-27 21:14:38 -0300

    chore: update tailwindcss config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,5 +16,4 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui')]
 };
