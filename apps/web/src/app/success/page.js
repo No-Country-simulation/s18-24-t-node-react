2aedcb8cb30a68cc69e6619ce2ec4171f956047e
guebebraga <guebe.braga5@gmail.com>
2024-10-31 14:54:42 -0300

    fix: fix import in property detail

--- apps/web/src/app/success/page.js
+++ apps/web/src/app/success/page.js
@@ -1,5 +1,4 @@
 "use client";
-import { HeaderBooked } from "@/app/components/headerBooked";
 import { useRouter } from "next/navigation";
 
 export default function Success() {
@@ -10,7 +9,6 @@ export default function Success() {
 
   return (
     <div>
-      <HeaderBooked />
       <div className="flex flex-col items-center justify-center bg-white min-h-screen text-black p-4">
         <h1 className="text-4xl font-bold mb-4 text-green-600">
           Payment Successful
